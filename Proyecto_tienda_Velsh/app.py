e13ce7094796dd6a2e98d46f085635f221a6007e
Vely2004 <nn.ceminarioa@uea.edu.ec>
2026-02-22 20:46:03 -0500

    semana10

--- Proyecto_tienda_Velsh/app.py
+++ Proyecto_tienda_Velsh/app.py
@@ -1,24 +1,54 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# Ruta de inicio
 @app.route('/')
 def inicio():
-    return "Bienvenido a Tienda Online Velsh - Calzado Femenino exclusivo y moderno"
+    return render_template("index.html")
 
-@app.route('/producto/<nombre>')
-def producto(nombre):
-    return f"Zapato modelo: {nombre} - Disponible en Tienda Velsh."
-
+# Rutas por categorías 
 @app.route('/categoria/<tipo>')
 def categoria(tipo):
-    return f"Categoría: {tipo} - Descubre nuestra colección en Velsh."
+    if tipo == "deportivos":
+        return render_template("deportivos.html")
+    elif tipo == "elegantes":
+        return render_template("elegantes.html")
+    elif tipo == "botas":
+        return render_template("botas.html")
+    elif tipo == "mocasines":
+        return render_template("mocasines.html")
+    else:
+        return "Categoría no encontrada", 404
+# Otras páginas
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 @app.route('/ofertas')
 def ofertas():
-    return "🔥 Ofertas especiales: 20% de descuento en artículos seleccionados."
+    return render_template("ofertas.html")
+
+@app.route('/pedido', methods=['GET', 'POST'])
+def pedido():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        email = request.form['email']
+        celular = request.form['celular']
+        direccion = request.form['direccion']
+        producto = request.form['producto']
+
+        return render_template(
+            "pedido_confirmado.html",
+            nombre=nombre,
+            producto=producto
+        )
+    return render_template("pedido.html")
+
+@app.route('/contacto')
+def contacto():
+    return render_template("contacto.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
